759c6767a8dc7fa957f7b43983e922b46b54c3a8
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 22:30:33 +0100

    Adding a WithError method to the base response for error handling

--- EldenRingTool/EldenRingFanApi/Communication/BaseResponse.cs
+++ EldenRingTool/EldenRingFanApi/Communication/BaseResponse.cs
@@ -3,6 +3,13 @@
 public class BaseResponse
 {
     public Error Error { get; set; }
+
+    public T WithError<T>(Error error) where T : BaseResponse
+    {
+        Error = error;
+
+        return (T) this;
+    }
 }
 
 public class Error
