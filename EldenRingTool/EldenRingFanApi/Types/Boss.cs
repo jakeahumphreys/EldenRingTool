83756003b658f2545d4f7b1913f1e389d0fc3443
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 22:24:51 +0100

    Adding a type to get all bosses from the ER Fan API

---
+++ EldenRingTool/EldenRingFanApi/Types/Boss.cs
@@ -0,0 +1,12 @@
+﻿namespace EldenRingTool.EldenRingFanApi.Types;
+
+public sealed class Boss
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public string Drops { get; set; }
+    public string HealthPoints { get; set; }
+}
