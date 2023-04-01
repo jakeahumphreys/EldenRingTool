b7c85caf93140a64859a4e4d2db73e9d7a171370
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 22:47:31 +0100

    Spiking out a method to get all bosses from the ER Fan API

---
+++ EldenRingTool/EldenRingFanApi/FanApiClient.cs
@@ -0,0 +1,35 @@
+﻿using EldenRingTool.EldenRingFanApi.Communication;
+using EldenRingTool.EldenRingFanApi.Types;
+using Newtonsoft.Json;
+
+namespace EldenRingTool.EldenRingFanApi;
+
+public sealed class FanApiClient
+{
+    private readonly HttpClient _httpClient;
+    private const string BaseUrl = "https://eldenring.fanapis.com/api/";
+
+    public FanApiClient()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public AllBossesResponse GetAll()
+    {
+        var uri = new Uri($"{BaseUrl}/bosses");
+        var responseString = _httpClient.GetStringAsync(uri).Result;
+
+        if (string.IsNullOrWhiteSpace(responseString))
+            return new AllBossesResponse().WithError<AllBossesResponse>(new Error{Message = "Unable to fetch bosses."});
+
+        var parsedResponse = JsonConvert.DeserializeObject<List<Boss>>(responseString);
+
+        if (parsedResponse.Count == 0)
+            return new AllBossesResponse().WithError<AllBossesResponse>(new Error {Message = "No bosses were returned from the API."});
+
+        return new AllBossesResponse
+        {
+            Bosses = parsedResponse
+        };
+    }
+}
