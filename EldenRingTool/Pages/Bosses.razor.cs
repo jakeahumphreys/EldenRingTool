68b538155f6c4deef3539a6319e3f35f90f70ee7
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 23:23:43 +0100

    Spiked out a test page for the All bosses call

---
+++ EldenRingTool/Pages/Bosses.razor.cs
@@ -0,0 +1,25 @@
+﻿using EldenRingTool.EldenRingFanApi;
+using EldenRingTool.EldenRingFanApi.Types;
+using Microsoft.AspNetCore.Components;
+
+namespace EldenRingTool.Pages;
+
+public partial class Bosses
+{
+    [Inject]
+    protected FanApiService FanApiService { get; set; }
+    
+    public List<Boss> AllBosses { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var allBossesResponse = FanApiService.GetAll();
+
+        if (allBossesResponse.Error != null)
+        {
+            //handle error
+        }
+
+        AllBosses = allBossesResponse.Bosses;
+    }
+}
