07f99755cc001392f2a17415264167e6da044046
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 22:51:16 +0100

    Adding a service-layer class for the ER Fan API

---
+++ EldenRingTool/EldenRingFanApi/FanApiService.cs
@@ -0,0 +1,18 @@
+﻿using EldenRingTool.EldenRingFanApi.Communication;
+
+namespace EldenRingTool.EldenRingFanApi;
+
+public sealed class FanApiService
+{
+    private readonly FanApiClient _fanApiClient;
+
+    public FanApiService()
+    {
+        _fanApiClient = new FanApiClient();
+    }
+
+    public AllBossesResponse GetAll()
+    {
+        return _fanApiClient.GetAll();
+    }
+}
