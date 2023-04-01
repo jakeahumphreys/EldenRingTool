fc616ed37e1a3534c38767202b6a9b69e26d9545
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 22:15:22 +0100

    Initial commit

---
+++ EldenRingTool/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EldenRingTool;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ EldenRingTool/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EldenRingTool;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
