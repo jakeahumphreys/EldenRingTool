4ea246a48e3c467acff8408e925f791e48656d9f
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 22:35:25 +0100

    Adding a response object to get all bosses from the ER fan api

---
+++ EldenRingTool/EldenRingFanApi/Communication/AllBossesResponse.cs
@@ -0,0 +1,8 @@
+﻿using EldenRingTool.EldenRingFanApi.Types;
+
+namespace EldenRingTool.EldenRingFanApi.Communication;
+
+public sealed class AllBossesResponse : BaseResponse
+{
+    public List<Boss> Bosses { get; set; }
+}
