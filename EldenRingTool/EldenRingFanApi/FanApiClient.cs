f1a6fa1413fb144f274e823275dbc72843ed9322
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 23:22:59 +0100

    Corrections this class that I found were breaking the functionality

--- EldenRingTool/EldenRingFanApi/FanApiClient.cs
+++ EldenRingTool/EldenRingFanApi/FanApiClient.cs
@@ -7,7 +7,7 @@ namespace EldenRingTool.EldenRingFanApi;
 public sealed class FanApiClient
 {
     private readonly HttpClient _httpClient;
-    private const string BaseUrl = "https://eldenring.fanapis.com/api/";
+    private const string BaseUrl = "https://eldenring.fanapis.com/api";
 
     public FanApiClient()
     {
@@ -22,14 +22,14 @@ public sealed class FanApiClient
         if (string.IsNullOrWhiteSpace(responseString))
             return new AllBossesResponse().WithError<AllBossesResponse>(new Error{Message = "Unable to fetch bosses."});
 
-        var parsedResponse = JsonConvert.DeserializeObject<List<Boss>>(responseString);
+        var parsedResponse = JsonConvert.DeserializeObject<BossesRoot>(responseString);
 
-        if (parsedResponse.Count == 0)
+        if (parsedResponse.Data.Count == 0)
             return new AllBossesResponse().WithError<AllBossesResponse>(new Error {Message = "No bosses were returned from the API."});
 
         return new AllBossesResponse
         {
-            Bosses = parsedResponse
+            Bosses = parsedResponse.Data
         };
     }
 }
