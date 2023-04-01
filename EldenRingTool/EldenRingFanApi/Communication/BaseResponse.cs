eb8d1f161c7de3151cf50331192063ea4796a420
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-01 22:28:37 +0100

    Setting up framework objects for communication to the ER Fan API. The base response will be shared amongst any other response objects

---
+++ EldenRingTool/EldenRingFanApi/Communication/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace EldenRingTool.EldenRingFanApi.Communication;
+
+public class BaseResponse
+{
+    public Error Error { get; set; }
+}
+
+public class Error
+{
+    public string Message { get; set; }
+}
